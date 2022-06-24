e8776ac83eb0d956bd49753f169c5464665f1523
Jared Shelby <jared.shelby@yale.edu>
2022-06-24 11:46:44 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: "Jared", last_name: "Shelby")
+Student.create(first_name: "Jerod", last_name: "Sellbie")
+Student.create(first_name: "Jorad", last_name: "Soolbuth")
